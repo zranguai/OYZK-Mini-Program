d1a4a621ae9f0c22c1f80934cd00a36ca50c7843
zranguai <zranguai@gmail.com>
2020-07-26 16:12:14 +0800

    网络请求及封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,5 @@
 // pages/home/home.js
+import request from '../../service/network.js'
 Page({
 
   /**
@@ -12,7 +13,35 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 1.原生
+    // get请求
+    // wx.request({
+    //   url: 'http://152.136.185.210:8000/api/n3/recommend',
+    //   success: function(res){
+    //     console.log(res);
+        
+    //   }
+    // })
+    // post请求
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: 'post',
+    //   data: {
+    //     name: 'zhangsan',
+    //     age: 18
+    //   },
+    //   success: res=> {
+    //     console.log(res);
+    //   }
+    // })
+    // 2.封装
+    request({
+      url: 'http://152.136.185.210:8000/api/n3/recommend'
+      }).then(res=>{
+      console.log(res);
+    }).catch(err=>{
+      console.log(err);
+    })
   },
 
   /**
